e7fc3dbf7a0e654db7fafa4db480863f593c497f
Ubuntu <peter@uskee.org>
2015-11-25 18:09:36 +0800

    update genhosts.sh

--- genhosts.sh
+++ genhosts.sh
@@ -2,8 +2,10 @@
 
 source $ZBASH
 
-gen_hosts() {
+gen_hosts() 
+{
     [ $# -ne 1 ] && return 1
+    [ -z "$1" ] && return 1
     [ ! -f server.ini ] && return 1
 
     local etc_hosts="$1"
@@ -26,8 +28,30 @@ gen_hosts() {
     echo "## custom hosts end" >> $etc_hosts
 }
 
+gen_example() 
+{
+    [ -f ./server.ini ] && return 0
+    cat > ./server.ini << EOF
+[bogon1]
+host = 192.168.175.135
+user = peter
 
-[ $# -ne 1 ] && tmp="/tmp/hosts.extra" || tmp="$1"
-gen_hosts "$tmp"
+[bogon2]
+host = 192.168.175.136
+user = peter
+
+
+[@all]
+host = bogon1, bogon2
+EOF
+}
+
+
+[ $# -eq 1 ] && tmp="$1"
+if [ "$tmp" = "help" ]; then
+    gen_example
+else
+    gen_hosts "$tmp"
+fi
 
 exit 0
