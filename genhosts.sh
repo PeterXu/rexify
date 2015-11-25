0673033a4d8bb1b330410dc1b8148f75f45dd6b9
Ubuntu <peter@uskee.org>
2015-11-25 17:30:23 +0800

    update genhosts.sh

--- genhosts.sh
+++ genhosts.sh
@@ -2,29 +2,31 @@
 
 source $ZBASH
 
-cfg_hosts() {
-    mapset hosts "bogon1" "192.168.175.131"
-    mapset hosts "bogon2" "192.168.175.132"
-}
-
-
 gen_hosts() {
     [ $# -ne 1 ] && return 1
+    [ ! -f server.ini ] && return 1
+
     local etc_hosts="$1"
     local etc_key="## custom hosts"
-    local hosts=$(mapkey hosts)
-
     echo "" > $etc_hosts
     echo "$etc_key" >> $etc_hosts
-    for h in $hosts; do
-        echo "$(mapget hosts $h)  $h" >> $etc_hosts
+
+    ini-parse server.ini
+    local keys=$(ini-secs server.ini)
+    for key in $keys; do
+        if [[ "$key" =~ "@" ]]; then
+            host=$(mapget $key host)
+            echo "#${key:1} = $host" >> $etc_hosts
+        else
+            host=$(mapget $key host)
+            user=$(mapget $key user)
+            echo "$host  $key" >> $etc_hosts
+        fi
     done
 }
 
 
-if [ $# -eq 1 ]; then
-    cfg_hosts
-    gen_hosts "$1"
-fi
+[ $# -ne 1 ] && tmp="/tmp/hosts.extra" || tmp="$1"
+gen_hosts "$tmp"
 
 exit 0
