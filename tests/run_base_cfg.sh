47d7fb2f716da98519b4590114fa2dab006c4599
Ubuntu <peter@uskee.org>
2015-12-15 16:31:31 +0800

    add once-run script

---
+++ tests/run_base_cfg.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+[ $# -ne 1 ] && exit 1
+grp="$1"
+ruser="$RUSER"
+[ "$grp" = "" -o "$ruser" = "" ] && exit 1
+
+
+echo "=========================="
+echo "[sshkey]"
+rex -G $grp Service:sshkey:prepare
+
+echo "=========================="
+echo "[apt]"
+rex -G $grp Service:apt:prepare
+
+echo "=========================="
+echo "[docker]"
+rex -G $grp Service:docker:prepare
+
+echo "=========================="
+echo "[pip]"
+rex -G $grp Service:pip:prepare
+#rex -G $grp Service:hosts:prepare
+
+echo "=========================="
+echo "[pull dockerfile]"
+cmd="git clone https://github.com/peterxu/docker.git ~/docker"
+rex -G $grp Service:manual:custom --by=run --cmd="$cmd"
+
+echo "=========================="
+echo "[docker-compose]"
+cmd="pip install docker-compose"
+rex -G $grp Service:manual:custom --by=run --cmd="$cmd"
+
+exit 0
