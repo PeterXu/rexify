e6ab85f8385e81a0ef79e6d5b242a6bad1bc1b09
Ubuntu <peter@uskee.org>
2016-02-27 13:59:38 +0800

    update

--- tests/set_host.sh
+++ tests/set_host.sh
@@ -1,23 +1,22 @@
 [ $# -ne 2 ] && echo "[usage] $0 hostname ipaddr" && exit 1
 
+## step1: set hostname
 host="$1"
 echo "[*] set hostname: <$host>"
 sed -in "s/^127.0.1.1.*/127.0.1.1    $host/" /etc/hosts
-
 cat <<EOF > /etc/hostname
 $host
 EOF
 
 
-if [ ${#2} -ge 4 ]; then
+## step2: set ip address
+dns="10.11.210.253"
+[ ${#2} -lt 4 ] && echo "invalid ipaddr" && exit 1
 ipaddr="$2"
-else
-ipaddr="10.11.200.$2"
-fi
 OFS="$IFS" && IFS="." && ips=($ipaddr) && IFS="$OFS"
 ippre="${ips[0]}.${ips[1]}.${ips[2]}"
 
-echo "[*] set ipaddr: <$ipaddr>"
+echo "[*] set ipaddr: <$ipaddr> dns: <$dns>"
 cat <<EOF > /etc/network/interfaces
 # This file describes the network interfaces available on your system
 # and how to activate them. For more information, see interfaces(5).
@@ -36,9 +35,8 @@ broadcast ${ippre}.255
 gateway ${ippre}.254
 
 # dns-* options are implemented by the resolvconf package, if installed
-dns-nameservers 10.11.40.5 202.102.192.68
+dns-nameservers ${dns} 202.102.192.68
 dns-search sportsdata.cn
 EOF
 
-echo "[*] set ok!"
 echo
